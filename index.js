b9b5537dbb9dace7c39e8e91935537d61e3742a0
Sagar Kumar <sagarkumar1952115>
2023-01-15 21:39:55 +0530

    added authorization header

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const app = express();
-const Octokit = require("octokit");
+
 const api = require("./routes/api");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
-// app.use(bodyParser.json());
-
-// const octokit = new Octokit({
-//   auth: "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",
-// });
-
-// await octokit.request("GET /rate_limit", {});
-// app.use(cors());
+const app = express();
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- routes/api.js
+++ routes/api.js
@@ -10,12 +10,11 @@ router.get("/github/userinfo/:user/repos", async function (req, res) {
     path: "/users/" + user + "/repos",
 
     headers: {
+      Authorization:
+        "Bearer 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",
+
       "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36",
-    },
-    Authorization: {
-      Token:
-        "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",
     },
   };
   https
@@ -35,12 +34,10 @@ router.get("/github/userinfo/:user", async function (req, res) {
     hostname: "api.github.com",
     path: "/users/" + user,
     headers: {
+      Authorization:
+        "Bearer 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",
       "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36",
-    },
-    Authorization: {
-      Token:
-        "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",
     },
   };
   https
@@ -66,6 +63,27 @@ router.get("/github/repoinfo/:user/:reponame", async function (req, res) {
     Authorization: {
       Token:
         "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",
+    },
+  };
+  https
+    .get(options, function (apiResponse) {
+      apiResponse.pipe(res);
+    })
+    .on("error", (e) => {
+      console.log(e);
+      res.status(500).send("Something wnent wrong!");
+    });
+});
+
+router.get("/github/rate_limit", async function (req, res) {
+  const options = {
+    hostname: "api.github.com",
+    path: "/rate_limit",
+    headers: {
+      Authorization:
+        "Bearer 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",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36",
     },
   };
   https
